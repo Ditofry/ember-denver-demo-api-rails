9269545311f88a31dfe3dedd2e1171890d7dd1d6
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-01-25 23:35:39 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+emburritos = [
+  [ "Kyle Coberly", "Web app developer, business dork, amateur economist." ],
+  [ "Brett Barrett", "Graduated with a bachelor's degree in Chemical and Biological Engineering, and now I do webdesign and developement. I love programming!" ]
+]
+
+emburritos.each do |name, introduction|
+  Emburrito.create( name: name, introduction: introduction )
+end
